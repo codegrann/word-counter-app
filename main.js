f7866514644b88ce95c32bd8f4369f9be2299740
codegrann <omondihills02@gmail.com>
2023-07-17 14:11:33 +0300

    selected required DOM elements and added event listeners of change of text input

--- main.js
+++ main.js
@@ -1,10 +1,12 @@
 let text=document.querySelector('#text');
-let wordCount=document.querySelector('#words');
-let charCount=document.querySelector('characters');
 
 text.addEventListener('input', updateCounter);
 
 function updateCounter(e){
+    let wordCount=document.querySelector('#words');
+    let charCount=document.querySelector('#characters');
     let sentence=e.target.value;
     console.log(sentence);
+    console.log(charCount.textContent);
+    console.log(wordCount.textContent);
 }
