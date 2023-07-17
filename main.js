1c7140dabe73d06a30381b42cbd3ca4f864a5bbf
codegrann <omondihills02@gmail.com>
2023-07-17 14:06:49 +0300

    .

--- main.js
+++ main.js
@@ -2,8 +2,9 @@ let text=document.querySelector('#text');
 let wordCount=document.querySelector('#words');
 let charCount=document.querySelector('characters');
 
-text.addEventListener('click', updateCounter);
+text.addEventListener('input', updateCounter);
 
-function updateCounter(){
-    text.style.backgroundColor='red';
+function updateCounter(e){
+    let sentence=e.target.value;
+    console.log(sentence);
 }
