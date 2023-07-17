af1f841fc99b9032bac6e5ef1f9648e468009f2f
codegrann <omondihills02@gmail.com>
2023-07-17 14:53:33 +0300

    implemented a regular expression to check for words

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ function updateCounter(e){
     let wordCount=document.querySelector('#words');
     let charCount=document.querySelector('#characters');
     let sentence=e.target.value;
-    let regex=/\s*(\w+ | \W+)\s*/g;
+    let regex=/(^\w+)|(\w+\S+)|(\s\w+)|(\s\W+)/g;
     charCount.textContent=sentence.length;
     wordCount.textContent=regex.test(sentence)? sentence.match(regex).length:0;
 
