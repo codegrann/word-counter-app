70fb60935d69a75a3d1c97c0182375707829a8d2
codegrann <omondihills02@gmail.com>
2023-07-17 14:59:23 +0300

    .

--- main.js
+++ main.js
@@ -6,10 +6,12 @@ function updateCounter(e){
     let wordCount=document.querySelector('#words');
     let charCount=document.querySelector('#characters');
     let sentence=e.target.value;
+
+    // regex to check for number of words
     // let regex=/(^\w+)|(\w+\S+)|(\s\w+)|(\s\W+)/g;
     let regex=/(\S+\b)/g;
+    
+    // updating word and character counter
     charCount.textContent=sentence.length;
     wordCount.textContent=regex.test(sentence)? sentence.match(regex).length:0;
-
-    console.log(sentence);
 }
