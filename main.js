a2ac556dc05e1b6f0a5baae757c31a50912ea355
codegrann <omondihills02@gmail.com>
2023-07-17 13:58:08 +0300

    .

--- main.js
+++ main.js
@@ -1,3 +1,10 @@
 let text=document.querySelector('#text');
 let wordCount=document.querySelector('#words');
 let charCount=document.querySelector('characters');
+
+text.addEventListener('click', updateCounter);
+
+function updateCounter(){
+    text.style.backgroundColor='red';
+    text.innerHTML='<p>Am clicked</p>';
+}
