8148f6e4c353d63719b0cc43a4f290c433f9aaf7
codegrann <omondihills02@gmail.com>
2023-07-17 14:54:46 +0300

    .

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ function updateCounter(e){
     let wordCount=document.querySelector('#words');
     let charCount=document.querySelector('#characters');
     let sentence=e.target.value;
-    let regex=/(^\w+)|(\w+\S+)|(\s\w+)|(\s\W+)/g;
+    let regex=/(^\w+)|(\w+\S+)|(\s\w+)|(\s\W+)/gi;
     charCount.textContent=sentence.length;
     wordCount.textContent=regex.test(sentence)? sentence.match(regex).length:0;
 
